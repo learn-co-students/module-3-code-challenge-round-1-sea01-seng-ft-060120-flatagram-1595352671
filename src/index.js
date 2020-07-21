f043c005edcb6720f0f843175e844963bbe0aa68
zanhe <zhertica@gmail.com>
2020-07-21 11:59:27 -0700

    3codech

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // write your code here
+
+
+fetch('http://localhost:3000/images/1')
+    .then(res => res.json())
+    .then(json => renderImage(json))
+
+
+const renderImage = (images) => {
+    
+    let title = document.getElementsByClassName('title')
+
+    title.innerHTML = `${images.title}`
+
+    let likes = document.getElementsByClassName('likes-section')
+    
+    likes.innerHTML = `${images.likes}`
+  
+}
+
